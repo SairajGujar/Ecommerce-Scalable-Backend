9c277a60b98f09693423d1b460dec53dc6b3a5a0
SAiii0101 <sairajgujar2000@gmail.com>
2025-01-04 00:09:11 +0530

    integrating docker

--- gateway/server.js
+++ gateway/server.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const expressProxy = require('express-http-proxy');
+import express from 'express';
+import expressProxy from 'express-http-proxy';
 
 const app = express();
 
-app.use('/user', expressProxy('http://localhost:3001'));
-
-app.use('/products', expressProxy('http://localhost:3002'));
-
-app.use('/cart', expressProxy('http://localhost:3003'));
-
-app.use('/orders', expressProxy('http://localhost:3004'));
-
-app.use('/notification', expressProxy('http://localhost:3005'));
+app.use('/user', expressProxy('http://user:3001'));
+app.use('/products', expressProxy('http://product-service:3002'));
+app.use('/cart', expressProxy('http://shopping-cart:3003'));
+app.use('/orders', expressProxy('http://order-service:3004'));
+app.use('/notification', expressProxy('http://notification-service:3005'));
 
 app.listen(3000, () => {
   console.log(`Gateway running on http://localhost:3000`);
